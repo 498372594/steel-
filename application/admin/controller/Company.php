2f5261ae26db88b2cc07e622634c92a2938934ae
韩韩韩 <945120753@qq.com>
2019-02-28 09:25:55 +0800

    增加企业管理

--- application/admin/controller/Company.php
+++ application/admin/controller/Company.php
@@ -12,6 +12,12 @@ class Company extends Right
 
     /**
      * 添加 验证前处理
+     * @param $data
+     * @return mixed
+     * @throws Exception
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
      */
     protected function afterAddValidate($data)
     {
@@ -26,6 +32,46 @@ class Company extends Right
             }
         }
         $data['create_time'] = now_datetime();
+        return $data;
+    }
+
+    /**
+     * 编辑附加数据
+     * @throws Exception
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    protected function editAttach()
+    {
+        $id = input("id");
+        if (empty($id))  throw new Exception("未知的id！");
+
+        $data = Db::table("company m")->where("m.id", $id)->find();
+        $this->assign("data", $data);
+    }
+
+    /**
+     * 编辑 验证前处理
+     * @param $data
+     * @return mixed
+     * @throws Exception
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    protected function beforeEditValidate($data)
+    {
+        if ($data['name']) {
+            if (Db::table("company")->where("name", $data['name'])->find()) {
+                throw new Exception("公司名称已存在！");
+            }
+        }
+        if ($data['phone']){
+            if(!isPhone($data['phone'])){
+                throw new Exception("手机号码格式有误！");
+            }
+        }
         return $data;
     }
 }
