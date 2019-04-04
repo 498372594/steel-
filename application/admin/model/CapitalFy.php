e61eae20855ccfd15cd0c5fe60f393fcc8b985c4
LvZhao <abc-1.2@qq.com>
2019-04-04 11:07:16 +0800

    修改关联，完善费用处理逻辑

--- application/admin/model/CapitalFy.php
+++ application/admin/model/CapitalFy.php
@@ -11,4 +11,21 @@ class CapitalFy extends Base
     use DeletePlugin;
     protected $autoWriteTimestamp = true;
 
+    public function szmcData()
+    {
+        return $this->belongsTo('Paymenttype', 'shouzhimingcheng_id', 'id')->cache(true, 60)
+            ->field('id,class,name')->bind(['szfl_name' => 'class', 'szmc_name' => 'name']);
+    }
+
+    public function pjlxData()
+    {
+        return $this->belongsTo('Pjlx', 'piaoju_id', 'id')->cache(true, 60)
+            ->field('id,pjlx')->bind(['pjlx_name' => 'pjlx']);
+    }
+
+    public function custom()
+    {
+        return $this->belongsTo('Custom', 'customer_id', 'id')->cache(true, 60)
+            ->field('id,custom')->bind(['dfdw_name' => 'custom']);
+    }
 }
