6bad6c1dc3c32179c29cedb59e03983c0b4f36ac
shc <513300023@qq.com>
2019-04-04 11:15:03 +0800

    tijiaownejian

--- application/admin/model/CapitalFy.php
+++ application/admin/model/CapitalFy.php
@@ -18,7 +18,6 @@ class CapitalFy extends Base
 
     // 验证错误信息
     public $msg = [
-
     ];
 
     // 场景
@@ -32,24 +31,4 @@ class CapitalFy extends Base
     ];
 }
 
-<<<<<<< HEAD
-=======
-    public function szmcData()
-    {
-        return $this->belongsTo('Paymenttype', 'shouzhimingcheng_id', 'id')->cache(true, 60)
-            ->field('id,class,name')->bind(['szfl_name' => 'class', 'szmc_name' => 'name']);
-    }
 
-    public function pjlxData()
-    {
-        return $this->belongsTo('Pjlx', 'piaoju_id', 'id')->cache(true, 60)
-            ->field('id,pjlx')->bind(['pjlx_name' => 'pjlx']);
-    }
-
-    public function custom()
-    {
-        return $this->belongsTo('Custom', 'customer_id', 'id')->cache(true, 60)
-            ->field('id,custom')->bind(['dfdw_name' => 'custom']);
-    }
-}
->>>>>>> e61eae20855ccfd15cd0c5fe60f393fcc8b985c4
